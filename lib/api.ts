0a31d3ea5f39d637815ed970ee5d103fbb251e96
Matt Sutkowski <msutkowski@gmail.com>
2021-10-23 12:26:42 -0700

    Setup createApi, store, hooks

---
+++ lib/api.ts
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { HYDRATE } from "next-redux-wrapper";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://pokeapi.co/api/v2/",
+  }),
+  extractRehydrationInfo(action) {
+    if (action.type === HYDRATE) {
+      return action.payload.api;
+    }
+  },
+  tagTypes: [],
+  endpoints: (builder) => ({
+    getPokemonByName: builder.query<
+      { species: { name: string }; sprites: { front_shiny: string } },
+      string
+    >({
+      query: (name) => `pokemon/${name}`,
+    }),
+    getPokemonList: builder.query<{ results: Array<{ name: string }> }, void>({
+      query: () => `pokemon/`,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components
+export const {
+  useGetPokemonByNameQuery,
+  useGetPokemonListQuery,
+  getRunningOperationPromises,
+} = api;
+
+// export endpoints for use in SSR
+export const { getPokemonByName, getPokemonList } = api.endpoints;
